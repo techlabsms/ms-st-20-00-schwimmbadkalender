f7fc7e5e50a962e6babdb940f24f54fb2f701c27
ThomasDroege <ThomasDroege@gmx.de>
2020-06-06 11:52:31 +0200

    added get endpoints

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // load our app server using express
 const express = require('express')
-const app = express()
+
+
+app.use(morgan('short'))
+
+app.get("/", (req, res) => {
+    console.log("Responding to root route")
+    res.send("Hello from ROOOTT")
+})
+
+app.get("/users", (req, res) => {
+    const user1 = {firstName: "Thomas", lastName: "Dröge"}
+    const user2 = {firstName: "Lina", lastName: "Fromme"}
+    const user3 = {firstName: "Charlotte", lastName: "Ausborn"}
+    
+    res.json([user1, user2, user3])
+})
 
 // localhost:3000 on browser
 app.listen(3000, () => {
